50101b1777d9b87072e0cb22559c7816f94bd523
SinnerSS <ntnbusinf@gmail.com>
2023-11-21 00:48:46 +0700

    final version

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var controller = new ScrollMagic.Controller();
+const section = document.querySelector('#title');
 
-  // Create a scene for each section
-  document.querySelectorAll('.section').forEach(function (section) {
-    new ScrollMagic.Scene({
-      triggerElement: section,
-      triggerHook: 0.8, // Change this value to adjust when the animation starts
-      reverse: false
-    })
-    .setClassToggle(section, 'visible') // Add a class when section enters the viewport
-    .addTo(controller);
+// Options for the Intersection Observer
+const options = {
+  root: null, // Use the viewport as the root
+  rootMargin: '0px', // No margin around the root
+  threshold: 0.5 // Trigger when 50% of the section is visible
+};
+
+// Intersection Observer callback function
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible'); // Add 'visible' class when section is in view
+      observer.unobserve(entry.target); // Stop observing once the section is visible
+    }
   });
-});
+};
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver(callback, options);
+
+// Start observing the section
+observer.observe(section);
 
